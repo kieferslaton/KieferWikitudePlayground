053b501944646f1957df28afccec6d03c96df2af
Keifer Slaton <kslaton@kslaton-m1.local>
2022-11-28 15:22:19 -0600

    added montserrat font and made main menu buttons circular, still working on bug with color change app

--- Assets/Samples/Wikitude SDK Core/9.10.0/Samples/Scripts/ColorPaletteController.cs
+++ Assets/Samples/Wikitude SDK Core/9.10.0/Samples/Scripts/ColorPaletteController.cs
@@ -8,11 +8,25 @@ public class ColorPaletteController : MonoBehaviour
 {
     [SerializeField] ImageTrackable trackableToChange;
 
+    private ImageTracker trackableParent;
+    private GameObject referenceObject;
+
+    void Awake()
+    {
+        trackableParent = trackableToChange.transform.parent.gameObject.GetComponent<ImageTracker>();
+        referenceObject = trackableToChange.Drawable;
+    }
+
     public void OnColorClicked(Button sender)
     {
+        Debug.Log(GetComponentInChildren<Image>().color);
         trackableToChange.enabled = false;
-        Debug.Log(sender.GetComponentInChildren<Image>().color);
-        trackableToChange.Drawable.gameObject.GetComponent<Renderer>().material.SetColor("_Color", sender.GetComponentInChildren<Image>().color);
+        ImageTrackable newTrackable = trackableParent.gameObject.AddComponent(typeof(ImageTrackable)) as ImageTrackable;
+        newTrackable.TargetPattern = trackableToChange.TargetPattern;
+        referenceObject.gameObject.GetComponent<Renderer>().material.SetColor("_Color", sender.GetComponentInChildren<Image>().color);
+        Destroy(trackableToChange);
+        newTrackable.Drawable = referenceObject;
+        trackableToChange = newTrackable;
         trackableToChange.enabled = true;
     }
 }
